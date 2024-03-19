14505a3c098652fcf18dbd3a560a76ae1e375240
achiewe <mamukashviliachi706@gmail.com>
2024-03-19 13:55:52 +0400

    import darkMoon svg for header component and write the logic of the dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,39 @@
 import lightMoon from "../../public/assets/lightMoon.png";
+import darkMoon from "../../public/assets/darkMoon.png";
+import { useCountryStore } from "../store";
 
 export default function Header() {
+  const setDarkMode = useCountryStore((state) => state.setDarkMode);
+  const darkMode = useCountryStore((state) => state.darkMode);
   return (
-    <div className="w-full flex flex-row items-center justify-between bg-[#ffffff] px-[16px] py-[30px] shadow-sm mb-[30px]">
-      <h1 className="font-extrabold text-[14px] leading-[20px]">
+    <div
+      className={`w-full flex flex-row items-center justify-between ${
+        darkMode ? "bg-[#2B3844]" : "bg-[#ffffff]"
+      } px-[16px] py-[30px] shadow-sm mb-[30px]`}
+    >
+      <h1
+        className={`font-extrabold text-[14px] leading-[20px] ${
+          darkMode ? "text-[#ffffff]" : "text-[##111517]"
+        }`}
+      >
         Where in the world?
       </h1>
-      <div className="flex flex-row items-center gap-[10px]">
-        <img src={lightMoon} alt="light moon" className="w-[16px] h-[16px]" />
-        <h2 className="font-semibold text-[12px] leading-[16.37px]">
+      <div
+        className="flex flex-row items-center gap-[10px] cursor-pointer"
+        onClick={() => {
+          setDarkMode(!darkMode);
+        }}
+      >
+        <img
+          src={darkMode ? darkMoon : lightMoon}
+          alt="light moon"
+          className="w-[16px] h-[16px]"
+        />
+        <h2
+          className={`font-semibold text-[12px] leading-[16.37px]  ${
+            darkMode ? "text-[#ffffff]" : "text-[##111517]"
+          }`}
+        >
           Dark Mode
         </h2>
       </div>
