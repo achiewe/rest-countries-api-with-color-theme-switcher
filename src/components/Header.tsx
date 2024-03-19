d33932ca83655464161b35ad25f06f3ec422518d
achiewe <mamukashviliachi706@gmail.com>
2024-03-19 13:15:55 +0400

    import lightMoon ong in the public folder and import it in the header component and style full header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
+import lightMoon from "../../public/lightMoon.png";
+
 export default function Header() {
   return (
     <div className="w-full flex flex-row items-center justify-between bg-[#ffffff] px-[16px] py-[30px] shadow-sm">
       <h1 className="font-extrabold text-[14px] leading-[20px]">
         Where in the world?
       </h1>
+      <div className="flex flex-row items-center gap-[10px]">
+        <img src={lightMoon} alt="light moon" className="w-[16px] h-[16px]" />
+        <h2 className="font-semibold text-[12px] leading-[16.37px]">
+          Dark Mode
+        </h2>
+      </div>
     </div>
   );
 }
