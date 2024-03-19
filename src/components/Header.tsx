aed7411588b67a4c3a0a5c71d8a1262ef2ffe5f0
achiewe <mamukashviliachi706@gmail.com>
2024-03-19 13:01:33 +0400

    create h1 tag and style it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 export default function Header() {
   return (
-    <div className="w-full flex flex-row items-center justify-between bg-[#ffffff] px-[30px] py-[16px] shadow-sm"></div>
+    <div className="w-full flex flex-row items-center justify-between bg-[#ffffff] px-[16px] py-[30px] shadow-sm">
+      <h1 className="font-extrabold text-[14px] leading-[20px]">
+        Where in the world?
+      </h1>
+    </div>
   );
 }
