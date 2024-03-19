aa35a48618c57d732fd826c9eac399f12fc5c102
achiewe <mamukashviliachi706@gmail.com>
2024-03-19 15:47:03 +0400

    import search svg in the SearhcInput and also add padding left and right in the home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import SearchInput from "../components/Home/SearchInput";
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center gap-[30px]">
+    <div className="w-full flex flex-col items-center gap-[30px] px-[10px]">
       <SearchInput />
     </div>
   );
