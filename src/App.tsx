39f0bb6ad47833243755bb453ca4010336fbb52d
achiewe <mamukashviliachi706@gmail.com>
2024-03-19 14:00:24 +0400

    add the dark mode logic in the app tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import Header from "./components/Header";
+import { useCountryStore } from "./store";
 
 function App() {
+  const darkMode = useCountryStore((state) => state.darkMode);
   return (
-    <div className="w-full min-h-screen flex flex-col bg-[#fafafa]">
+    <div
+      className={`w-full min-h-screen flex flex-col ${
+        darkMode ? "bg-[#202C36]" : "bg-[#fafafa]"
+      }`}
+    >
       <Header />
     </div>
   );
