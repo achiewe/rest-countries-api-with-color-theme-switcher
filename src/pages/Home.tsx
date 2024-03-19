e2e4f7268eb6d41d9c7af20de73cbbfa840d543e
achiewe <mamukashviliachi706@gmail.com>
2024-03-19 15:39:27 +0400

    style home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
+import SearchInput from "../components/Home/SearchInput";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="w-full flex flex-col items-center gap-[30px]">
+      <SearchInput />
+    </div>
+  );
 }
