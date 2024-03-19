e39358bcae81035e490b863ec131f63b7fc287e7
achiewe <mamukashviliachi706@gmail.com>
2024-03-19 14:10:22 +0400

    use router and route for change the user interfae and alos add the property inside the route path and also element for change and go in the home tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import Header from "./components/Header";
+import Home from "./pages/Home";
 import { useCountryStore } from "./store";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   const darkMode = useCountryStore((state) => state.darkMode);
   return (
-    <div
-      className={`w-full min-h-screen flex flex-col ${
-        darkMode ? "bg-[#202C36]" : "bg-[#fafafa]"
-      }`}
-    >
-      <Header />
-    </div>
+    <Router>
+      <div
+        className={`w-full min-h-screen flex flex-col ${
+          darkMode ? "bg-[#202C36]" : "bg-[#fafafa]"
+        }`}
+      >
+        <Header />
+        <Route path="/" element={<Home />}></Route>
+      </div>
+    </Router>
   );
 }
 
