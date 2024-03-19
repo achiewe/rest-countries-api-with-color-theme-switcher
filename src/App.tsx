34a7215b88384810f4c6b0f53dcad440d0d3c92d
achiewe <mamukashviliachi706@gmail.com>
2024-03-19 12:57:27 +0400

    style the header component div and import it in the app tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-  return <div className="w-full min-h-screen flex flex-col bg-[#fafafa]"></div>;
+  return (
+    <div className="w-full min-h-screen flex flex-col bg-[#fafafa]">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="w-full flex flex-row items-center justify-between bg-[#ffffff] px-[30px] py-[16px] shadow-sm"></div>
+  );
 }
