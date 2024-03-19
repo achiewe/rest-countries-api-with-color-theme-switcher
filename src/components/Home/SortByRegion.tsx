78ec779c5bb0a6175989537204092c0eaa4b28ef
achiewe <mamukashviliachi706@gmail.com>
2024-03-19 18:01:06 +0400

    style h3 tag

--- src/components/Home/SortByRegion.tsx
+++ src/components/Home/SortByRegion.tsx
@@ -1,7 +1,9 @@
 export default function SortByRegion() {
   return (
     <div className="w-[200px] bg-[#ffffff] flex flex-grow justify-between items-center px-[14px] py-[19px]">
-      <h3> Filter by Region</h3>
+      <h3 className="font-normal text-[12px] leading-[20px]">
+        Filter by Region
+      </h3>
     </div>
   );
 }
