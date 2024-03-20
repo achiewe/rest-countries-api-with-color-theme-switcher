d104b90e6a8b1b842d517eab4ace0ed96a5f95fc
achiewe <mamukashviliachi706@gmail.com>
2024-03-20 15:39:42 +0400

    rename on the component name

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,10 +1,9 @@
-import FlagButton from "../components/Detail/FlagButton";
-import { useCountryStore } from "../store";
+import BackButton from "../components/Detail/BackButton";
 
 export default function Detail() {
   return (
     <div className="w-full flex flex-col gap-[30px] px-[20px] items-start justify-start">
-      <FlagButton />
+      <BackButton />
     </div>
   );
 }
