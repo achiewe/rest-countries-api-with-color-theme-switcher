88672ac620a03138bf8e00dd5ddfc7f5ed0ab2c2
achiewe <mamukashviliachi706@gmail.com>
2024-03-20 14:55:29 +0400

    create new state for input to take the value of input

--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,8 @@ import { CountryType } from "./type";
 type countryStore = {
   darkMode: boolean;
   fetchData: CountryType[];
+  inputValue: string;
+  setInputValue: (newValue: string) => void;
   setFetchData: (newData: CountryType[]) => void;
   setDarkMode: (newMode: boolean) => void;
 };
@@ -11,8 +13,12 @@ type countryStore = {
 export const useCountryStore = create<countryStore>((set) => ({
   darkMode: false,
   fetchData: [],
+  inputValue: "",
   setDarkMode: (newMode: boolean) => {
     set({ darkMode: newMode });
+  },
+  setInputValue: (newValue: string) => {
+    set({ inputValue: newValue });
   },
   setFetchData: (newData: CountryType[]) => {
     set({ fetchData: newData });
