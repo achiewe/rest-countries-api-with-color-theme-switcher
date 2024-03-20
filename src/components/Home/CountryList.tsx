b27655e1d97a6f418da7cf9a6fd8e2674632dad7
achiewe <mamukashviliachi706@gmail.com>
2024-03-20 13:12:22 +0400

    map contry from the data

--- src/components/Home/CountryList.tsx
+++ src/components/Home/CountryList.tsx
@@ -4,34 +4,43 @@ export default function CountryList() {
   console.log(Data);
   return (
     <div className="w-full flex flex-col justify-center items-center gap-[20px]">
-      <div className="w-[264px] rounded-[5px] flex flex-col shadow-lg pb-[46px] bg-[#ffffff] gap-[20px]">
-        <img className="w-full rounded-t-5" src="ragaa" alt="country flag" />
-        <div className="w-full flex flex-col items-start justify-start pl-[24px] gap-[20px]">
-          <h1 className="font-extrabold text-[18px] leading-[26px]">
-            country name
-          </h1>
-          <div className="w-full flex flex-col items-start justify-start gap-[10px]">
-            <h3 className="text-[#111517] text-[14px] font-semibold leading-[16px]">
-              Population:
-              <span className="text-[#111517] text-[14px] font-light leading-[16px]">
-                81,770,900
-              </span>
-            </h3>
-            <h3 className="text-[#111517] text-[14px] font-semibold leading-[16px]">
-              Region:
-              <span className="text-[#111517] text-[14px] font-light leading-[16px]">
-                81,770,900
-              </span>
-            </h3>
-            <h3 className="text-[#111517] text-[14px] font-semibold leading-[16px]">
-              Capital:
-              <span className="text-[#111517] text-[14px] font-light leading-[16px]">
-                81,770,900
-              </span>
-            </h3>
+      {Data.map((country) => (
+        <div
+          key={country.alpha3Code}
+          className="w-[264px] rounded-[5px] flex flex-col shadow-lg pb-[46px] bg-[#ffffff] gap-[20px]"
+        >
+          <img
+            className="w-full rounded-t-5"
+            src={country.flag}
+            alt="country flag"
+          />
+          <div className="w-full flex flex-col items-start justify-start pl-[24px] gap-[20px]">
+            <h1 className="font-extrabold text-[18px] leading-[26px]">
+              {country.name}
+            </h1>
+            <div className="w-full flex flex-col items-start justify-start gap-[10px]">
+              <h3 className="text-[#111517] text-[14px] font-semibold leading-[16px]">
+                Population:
+                <span className="text-[#111517] text-[14px] font-light leading-[16px]">
+                  {country.population}
+                </span>
+              </h3>
+              <h3 className="text-[#111517] text-[14px] font-semibold leading-[16px]">
+                Region:
+                <span className="text-[#111517] text-[14px] font-light leading-[16px]">
+                  {country.region}
+                </span>
+              </h3>
+              <h3 className="text-[#111517] text-[14px] font-semibold leading-[16px]">
+                Capital:
+                <span className="text-[#111517] text-[14px] font-light leading-[16px]">
+                  {country.capital}
+                </span>
+              </h3>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
