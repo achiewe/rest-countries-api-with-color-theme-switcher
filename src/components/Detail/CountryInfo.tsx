300349087b47443065e8ba028700b07d4e21cfa8
achiewe <mamukashviliachi706@gmail.com>
2024-03-20 16:33:20 +0400

    style CountryInfoComponent

--- src/components/Detail/CountryInfo.tsx
+++ src/components/Detail/CountryInfo.tsx
@@ -1,5 +1,68 @@
 export default function CountryInfo() {
   return (
-    <div className="w-full flex flex-col gap-[20px] items-start justify-start"></div>
+    <div className="w-full flex flex-col gap-[20px] items-start justify-start">
+      <img src="aaasd" alt="country flag" />
+      <div className="w-full flex flex-col gap-[30px] items-start justify-start">
+        <div className="flex flex-col gap-[30px] items-start justify-start">
+          <div className="flex flex-col gap-[20px] items-start justify-start">
+            <h1 className="font-extrabold text-[22px] leading-[30.01px] text-[#111517]">
+              belgium
+            </h1>
+            <div className="flex flex-col gap-[5px] items-start justify-start">
+              <h3 className="text-[14px] font-semibold leading-[32px] text-[#111517]">
+                Native Name:
+                <span className="text-[14px] font-normal leading-[32px]">
+                  België
+                </span>
+              </h3>
+              <h3 className="text-[14px] font-semibold leading-[32px] text-[#111517]">
+                Population:
+                <span className="text-[14px] font-normal leading-[32px]">
+                  1212122
+                </span>
+              </h3>
+              <h3 className="text-[14px] font-semibold leading-[32px] text-[#111517]">
+                Region:
+                <span className="text-[14px] font-normal leading-[32px]">
+                  Europe
+                </span>
+              </h3>
+              <h3 className="text-[14px] font-semibold leading-[32px] text-[#111517]">
+                Sub Region:
+                <span className="text-[14px] font-normal leading-[32px]">
+                  België
+                </span>
+              </h3>
+              <h3 className="text-[14px] font-semibold leading-[32px] text-[#111517]">
+                Capital:
+                <span className="text-[14px] font-normal leading-[32px]">
+                  België
+                </span>
+              </h3>
+            </div>
+          </div>
+          <div className="flex flex-col gap-[5px] items-start justify-start">
+            <h3 className="text-[14px] font-semibold leading-[32px] text-[#111517]">
+              Top Level Domain:
+              <span className="text-[14px] font-normal leading-[32px]">
+                België
+              </span>
+            </h3>
+            <h3 className="text-[14px] font-semibold leading-[32px] text-[#111517]">
+              Currencies:
+              <span className="text-[14px] font-normal leading-[32px]">
+                1212122
+              </span>
+            </h3>
+            <h3 className="text-[14px] font-semibold leading-[32px] text-[#111517]">
+              Languages:
+              <span className="text-[14px] font-normal leading-[32px]">
+                Europe
+              </span>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,9 +1,11 @@
 import BackButton from "../components/Detail/BackButton";
+import CountryInfo from "../components/Detail/CountryInfo";
 
 export default function Detail() {
   return (
     <div className="w-full flex flex-col gap-[30px] px-[20px] items-start justify-start">
       <BackButton />
+      <CountryInfo />
     </div>
   );
 }
