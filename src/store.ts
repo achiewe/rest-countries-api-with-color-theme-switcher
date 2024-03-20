d973e0c0bf74fc41bdc22d0fc8340624dc8c5f06
achiewe <mamukashviliachi706@gmail.com>
2024-03-20 14:39:36 +0400

    create new state for take the data info

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,20 @@
 import { create } from "zustand";
+import { CountryType } from "./type";
 
 type countryStore = {
   darkMode: boolean;
+  fetchData: CountryType[];
+  setFetchData: (newData: CountryType[]) => void;
   setDarkMode: (newMode: boolean) => void;
 };
 
 export const useCountryStore = create<countryStore>((set) => ({
   darkMode: false,
+  fetchData: [],
   setDarkMode: (newMode: boolean) => {
     set({ darkMode: newMode });
+  },
+  setFetchData: (newData: CountryType[]) => {
+    set({ fetchData: newData });
   },
 }));
