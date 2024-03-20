a93677e5e7bdb763c66dfc2449f7c2c4202cc445
achiewe <mamukashviliachi706@gmail.com>
2024-03-20 12:34:01 +0400

    create new component CountryList and import it in the home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import CountryList from "../components/Home/CountryList";
 import SearchInput from "../components/Home/SearchInput";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center gap-[30px] px-[10px]">
       <SearchInput />
+      <CountryList />
     </div>
   );
 }
