f8af5f120d2e1b40e6daf405190971f5934f5427
achiewe <mamukashviliachi706@gmail.com>
2024-03-20 17:13:09 +0400

    style div where should be border countries

--- src/components/Detail/CountryInfo.tsx
+++ src/components/Detail/CountryInfo.tsx
@@ -62,6 +62,18 @@ export default function CountryInfo() {
             </h3>
           </div>
         </div>
+        <div className="w-full flex flex-col gap-[15px] items-start justify-start">
+          <h3 className="text-[16px] font-semibold leading-[24px]">
+            Border Countries:
+          </h3>
+          <div className="w-full flex flex-row items-center justify-start gap-[5px]">
+            <div className="w-[96px] h-[28px] flex items-center justify-center border-2 border-gray-500 border-opacity-20">
+              <span className="text-[12px] font-light leading-[16.37px]">
+                country
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
