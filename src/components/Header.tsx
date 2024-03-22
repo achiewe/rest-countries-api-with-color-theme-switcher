246253ff43e31fb005cc4be5a2d0e5bf1e2769ca
achiewe <mamukashviliachi706@gmail.com>
2024-03-22 16:46:55 +0400

    add Link on the h1 tag to when onclick go to the home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,27 @@
 import lightMoon from "../../public/assets/lightMoon.png";
 import darkMoon from "../../public/assets/darkMoon.png";
 import { useCountryStore } from "../store";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const setDarkMode = useCountryStore((state) => state.setDarkMode);
   const darkMode = useCountryStore((state) => state.darkMode);
   return (
-    <div
+    <header
       className={`w-full flex flex-row items-center justify-between ${
         darkMode ? "bg-[#2B3844]" : "bg-[#ffffff]"
       } px-[16px] py-[30px] shadow-sm mb-[30px] md:px-[80px] md:py-[24px] md:mb-[40px] lg:mb-[70px]`}
     >
-      <h1
-        className={`font-extrabold text-[14px] leading-[20px] md:text-[20px] lg:text-[24px] ${
-          darkMode ? "text-[#ffffff]" : "text-[##111517]"
-        }`}
-      >
-        Where in the world?
-      </h1>
+      <Link to="/">
+        <h1
+          className={`font-extrabold text-[14px] leading-[20px] md:text-[20px] lg:text-[24px] cursor-pointer ${
+            darkMode ? "text-[#ffffff]" : "text-[##111517]"
+          }`}
+        >
+          Where in the world?
+        </h1>
+      </Link>
+
       <div
         className="flex flex-row items-center gap-[10px] cursor-pointer"
         onClick={() => {
@@ -37,6 +41,6 @@ export default function Header() {
           Dark Mode
         </h2>
       </div>
-    </div>
+    </header>
   );
 }
